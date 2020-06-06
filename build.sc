d1019647039635fa970a2a7919275af67a8e9545
itang <live.tang@gmail.com>
2020-06-06 23:18:07 +0800

    add go project support

--- build.sc
+++ build.sc
@@ -1,6 +1,11 @@
 // build.sc
 import mill._, scalalib._
+import coursier.maven.MavenRepository
 
 object donew_cli extends ScalaModule {
   def scalaVersion = "2.12.11"
+
+  def repositories = super.repositories ++ Seq(
+    MavenRepository("http://maven.aliyun.com/nexus/content/groups/public")
+  )
 }
--- donew_cli/src/scala/Main.scala
+++ donew_cli/src/scala/Main.scala
@@ -1,11 +1,16 @@
 package donew_cli
 
+import java.io.File
+
 object Main {
   private val DefaultDenoProjectName = "deno_test1"
+  private val DefaultGoProjectName = "go_test1"
 
   def main(args: Array[String]): Unit = args match {
     case Array("deno")       => _donewDeno(DefaultDenoProjectName)
     case Array("deno", name) => _donewDeno(name)
+    case Array("go")         => _donewGo(DefaultGoProjectName)
+    case Array("go", name)   => _donewGo(name)
     case _                   => println("any?")
   }
 
@@ -21,4 +26,67 @@ object Main {
 
     println(s"cd ${name} for your work!")
   }
+
+  private def _donewGo(name: String, args: String*): Unit = {
+    new GoProjectMaker(name, args).gen()
+  }
+}
+
+//TODO:
+trait ProjectMaker {
+  def gen(): Unit
+}
+
+abstract class AbstractProjectMaker extends ProjectMaker {
+  override def gen(): Unit = {
+    println(s"mkdir ${name}...")
+    val ok = root.mkdir()
+    println(s"mkdir ${name} ok?: ${ok}")
+    doWithShellInProjectDir()
+    doWithWriteFiles()
+
+    println(s"cd ${name} for your work!")
+  }
+
+  private def doWithShellInProjectDir(): Unit = {
+    for (cmd <- getShells()) {
+      val r = sys.process.Process(cmd, root).!!
+      println(r)
+    }
+  }
+
+  private def doWithWriteFiles() {
+    import java.nio.file.Files
+    for (FileWithContent(path, content) <- getFileWithContent()) {
+      println(s"write ${path}...")
+      Files.write(new File(root, path).toPath(), content.getBytes("utf-8"));
+    }
+  }
+
+  def root = new File(name)
+
+  def name: String
+
+  def args: Seq[String]
+
+  def getShells(): Seq[Seq[String]]
+
+  def getFileWithContent(): Seq[FileWithContent]
+}
+
+case class FileWithContent(path: String, content: String)
+class GoProjectMaker(val name: String, val args: Seq[String])
+    extends AbstractProjectMaker {
+  override def getShells =
+    Seq(Seq("go", "mod", "init", s"github.com/itang/${name}"))
+
+  override def getFileWithContent() =
+    Seq(FileWithContent("main.go", s"""package main
+
+import "fmt"
+
+func main(){
+    fmt.Println("Hello, World!")
+}
+"""))
 }
